9feebcf8a835956d43436239f8d7cb3f2206166c
Antoine GOHIN <contact@brokestudio.fr>
2025-07-06 13:19:54 +0200

    SDL2

--- Demo/Build-Demo.lua
+++ Demo/Build-Demo.lua
@@ -3,20 +3,20 @@ language "C++"
 cppdialect "C++17"
 targetdir "Binaries/%{cfg.buildcfg}"
 debugdir "Binaries/%{cfg.targetdir}"
-staticruntime "on"
+staticruntime "off"
 targetname "Demo"
 
 files {
-  "Source/**.h", "Source/**.cpp",
+  "./Source/**.h", "./Source/**.cpp",
 
-  "../External/SDL3/include/**.h",
+  "../External/SDL2/include/**.h",
   "../External/imgui/*.h", "../External/imgui/*.cpp",
   "../External/imgui/backends/**.h", "../External/imgui/backends/**.cpp",
 }
 
 vpaths {
-  ["SDL3"] = {
-    "../External/SDL3/include/**.h",
+  ["SDL2"] = {
+    "../External/SDL2/include/**.h",
   },
   ["ImGui"] = {
     "../External/imgui/*.h",
@@ -27,11 +27,11 @@ vpaths {
 }
 
 includedirs {
-  "Source",
+  "./Source",
 
   "../External/imgui",
   "../External/imgui/backends",
-  "../External/SDL3",
+  "../External/SDL2",
 }
 
 targetdir("../Binaries/" .. OutputDir .. "/%{prj.name}")
@@ -53,6 +53,9 @@ filter "configurations:Release"
   symbols "On"
 
 -- Windows
+
+filter { "system:windows" }
+  staticruntime "on"
 
 filter { "system:windows", "configurations:Dist" }
   kind "WindowedApp"
@@ -77,56 +80,49 @@ filter "system:windows"
   systemversion "latest"
   defines {
     "_CRT_SECURE_NO_WARNINGS",
-    -- "SDL_MAIN_HANDLED", -- to avoid SDL_main
+    "SDL_MAIN_HANDLED", -- to avoid SDL_main
   }
   includedirs {
-      "../External/SDL3/include"
+      "../External/SDL2/include"
   }
   links {
       "winmm.lib",
       "setupapi.lib",
       "version.lib",
       -- "Imm32.lib",
-      "opengl32",
-
-      "SDL3-static"
+      "opengl32"
   }
 
 filter { "system:windows", "configurations:Debug", "platforms:x86" }
-  libdirs {
-      "../External/SDL3/lib/x86-static-debug"
-  }
+  links { "SDL2-staticd" }
+  libdirs { "../External/SDL2/lib/x86-static-debug" }
 
 filter { "system:windows", "configurations:Debug", "platforms:x86_64" }
-  libdirs {
-      "../External/SDL3/lib/x64-static-debug"
-  }
+  links { "SDL2-staticd" }
+  libdirs { "../External/SDL2/lib/x64-static-debug" }
 
 filter { "system:windows", "configurations:Release or Dist", "platforms:x86" }
-  libdirs {
-      "../External/SDL3/lib/x86-static-release"
-  }
+  links { "SDL2-static" }
+  libdirs { "../External/SDL2/lib/x86-static-release" }
 
 filter { "system:windows", "configurations:Release or Dist", "platforms:x86_64" }
-  libdirs {
-      "../External/SDL3/lib/x64-static-release"
-  }
+  links { "SDL2-static" }
+  libdirs { "../External/SDL2/lib/x64-static-release" }
 
 -- Linux
 
 filter "system:linux"
-  includedirs {
-    -- "../External/SDL3/include",
-  }
+  buildoptions { "`sdl2-config --cflags`" }
+  linkoptions { "`sdl2-config --libs`" }
   links {
-    -- "GL",
+    "GL",
     -- "dl",
-    -- "pthread",
+    "pthread",
     "SDL2"
   }
 
 filter { "system:linux", "configurations:Dist" }
-  kind "WindowedApp"
+  kind "ConsoleApp"
   defines { "_DIST" }
   runtime "Release"
   optimize "On"
@@ -136,43 +132,27 @@ filter { "system:linux", "configurations:Dist" }
 -- macOS
 
 filter "system:macosx"
+  buildoptions { "`sdl2-config --cflags`" }
   linkoptions {
-    "-framework OpenGL -framework CoreFoundation",
-    "-framework CoreVideo -framework AudioToolbox -framework Carbon -framework IOKit",
-    "-framework Cocoa -framework ForceFeedback -framework CoreAudio",
-    "-framework Foundation -framework Metal",
-    "-framework GameController -framework CoreHaptics",
-    "-static-libsan",
-  }
-  links {
-    "pthread",
-    "m",
-    "dl",
-    "iconv",
-    "SDL3"
-  }
-  libdirs {
-    "../External/SDL3/lib/macOS"
-  }
-  includedirs {
-    "../External/SDL3/include",
-    "../macOS"
+    "`sdl2-config --libs`",
+    "-framework OpenGL",
+    "-framework CoreFoundation"
   }
 
-filter { "configurations:Dist", "system:macosx" }
+filter { "system:macosx", "configurations:Dist" }
   kind "ConsoleApp"
   defines { "_DIST" }
   runtime "Release"
   optimize "On"
   symbols "Off"
   targetdir("../Binaries/" .. OutputDir .. "/Demo")
-  postbuildcommands {
-    "{RMDIR} \"%{cfg.targetdir}/Demo.app\"",
-    "{MKDIR} \"%{cfg.targetdir}/Demo.app\"",
-    "{MKDIR} \"%{cfg.targetdir}/Demo.app/Contents\"",
-    "{MKDIR} \"%{cfg.targetdir}/Demo.app/Contents/MacOS\"",
-    "{MKDIR} \"%{cfg.targetdir}/Demo.app/Contents/Resources\"",
-    "{COPY} \"../macOS/Info.plist\" \"%{cfg.targetdir}/Demo.app/Contents\"",
-    "{COPY} \"%{cfg.targetdir}/Demo\" \"%{cfg.targetdir}/Demo.app/Contents/MacOS\"",
-    "{COPY} \"../macOS/Rainbow.png\" \"%{cfg.targetdir}/Demo.app/Contents/Resources\"",
-  }
+  -- postbuildcommands {
+  --   "{RMDIR} \"%{cfg.targetdir}/Demo.app\"",
+  --   "{MKDIR} \"%{cfg.targetdir}/Demo.app\"",
+  --   "{MKDIR} \"%{cfg.targetdir}/Demo.app/Contents\"",
+  --   "{MKDIR} \"%{cfg.targetdir}/Demo.app/Contents/MacOS\"",
+  --   "{MKDIR} \"%{cfg.targetdir}/Demo.app/Contents/Resources\"",
+  --   "{COPY} \"../macOS/Info.plist\" \"%{cfg.targetdir}/Demo.app/Contents\"",
+  --   "{COPY} \"%{cfg.targetdir}/Demo\" \"%{cfg.targetdir}/Demo.app/Contents/MacOS\"",
+  --   "{COPY} \"../macOS/Rainbow.png\" \"%{cfg.targetdir}/Demo.app/Contents/Resources\"",
+  -- }
---
+++ External/SDL2/include/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.32.8-6-g19fc5c295 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.32.8-6-g19fc5c295"
+#endif
